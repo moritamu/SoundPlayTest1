fa993a41fed11727802a3d59da5bb7317d44b383
morisum <moris@r.sannet.ne.jp>
2023-10-01 16:15:32 +0900

    Initial Commit

---
+++ SoundPlayTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundPlayTest1
+//
+//  Created by MsMacM on 2023/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundPlayTest1/SoundPlayTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  SoundPlayTest1App.swift
+//  SoundPlayTest1
+//
+//  Created by MsMacM on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct SoundPlayTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
